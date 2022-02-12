682c464641caea1d6743c34ebf9ac8393cdc1445
邹铈杰 <1106958765@qq.com>
2022-02-12 17:50:43 +0800

    11

--- src/page/Header/index.jsx
+++ src/page/Header/index.jsx
@@ -1,11 +1,18 @@
+import { Collapse } from 'antd';
 import './index.less'
+
+const { Panel } = Collapse;
 
 export default () => {
     return (
         <div className="header">
-            <div className="contain">
-                <img src="https://cdn.docschina.org/static/media/docschina-logo.4c5554f1.svg" alt="logo"></img>
-            </div>
+            <Collapse>
+                <Panel key="1" header="美妙的答题之旅">
+                  <div className="contain">
+                    <img src="https://cdn.docschina.org/static/media/docschina-logo.4c5554f1.svg" alt="logo"></img>
+                </div>   
+                </Panel>       
+            </Collapse>
         </div>
     )
 }
--- src/page/Html/index.jsx
+++ src/page/Html/index.jsx
@@ -1,5 +1,64 @@
+// @ts-nocheck
+import { useEffect, useState } from 'react';
+// @ts-ignore
+import md from '@/notes/html/01.md';
+import { Tree, Drawer, Button } from 'antd';
+import { DatabaseTwoTone } from '@ant-design/icons';
+import Markdown from 'react-markdown';
+import gfm from 'remark-gfm'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import './index.less';
+import {nanoid} from 'nanoid'
+
+let list = [];
+
 export default () => {
+    const [markdown, setMarkdown] = useState('');
+    const [visible, setVisible] = useState(false); // 导航栏抽屉
+    const showDrawer = () => {
+        setVisible(true);
+      };
+      const onClose = () => {
+        setVisible(false);
+      };
+    const components = {
+        code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+                <SyntaxHighlighter style={ dark } language={ match[1] } PreTag="div" children={ String(children).replace(/\n$/, '') } { ...props } />
+            ) : (
+                <code className={ className } { ...props } />
+            )
+        },
+        h3(props) {
+            list[list.length - 1].children.push({ title: props.children[0], key: nanoid() });
+            return <h3 style={ { color: 'red' } } { ...props } /> 
+        },
+        h2(props) {
+            list.push({ title:props.children[0],key:nanoid(),children:[] });
+            return <h2 { ...props } />
+        }
+    }
+
+    useEffect(() => {
+        fetch(md)
+            .then(res => res.text())
+            .then(text => setMarkdown(text))
+    }, [])
+
+    console.log(list,'list');
     return (
-        <div>html组件内容</div>
+        <div className="html-contain">
+            <Button type="primary" onClick={showDrawer} className="openbutton">
+            <DatabaseTwoTone/>
+            </Button>
+            <Drawer title="导航目录" placement="left" onClose={onClose} visible={visible}>
+              {list.length>1 && <Tree treeData={list} className="html-tree" />}
+            </Drawer>
+            <div>
+              <Markdown children={markdown} remarkPlugins={[gfm]} components={ components }/>  
+            </div>
+        </div>
     )
 }
